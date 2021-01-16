abb31ed54543032b128200a36301be57e9f9ecfd
Vrushti Shah <71961160+Vrushti24@users.noreply.github.com>
2021-01-16 15:14:32 +0530

    Update About.dart

--- lib/About.dart
+++ lib/About.dart
@@ -1,3 +1,4 @@
+import 'package:doclense/MainDrawer.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -5,23 +6,29 @@ class About extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueGrey[100],
+      
+      backgroundColor: Colors.white,
+      drawer: MainDrawer(),
       appBar: AppBar(
         title: Text(
           "About App",
           style: TextStyle(fontSize: 24, color: Colors.white),
         ),
-        backgroundColor: Colors.blue[600],
+     
+      backgroundColor: Colors.black,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
+          
+          Image.asset('assets/images/logos.png',width: 200,height: 100,),
+          
           Padding(
             padding: const EdgeInsets.fromLTRB(10, 40, 10, 10),
             child: Text(
               "DocLense is the one place for all your documents. \nYou can now click, upload, crop, rotate and do so much more!\n\nSo whether it is your college assignment or the office document you want to digitalize, stop worrying and just use DocLense!",
               style: TextStyle(
-                color: Colors.blue[600],
+                color: Colors.black,
                 fontSize: 18,
               ),
             ),
@@ -30,15 +37,18 @@ class About extends StatelessWidget {
             height: 200,
           ),
           Row(
+            
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.end,
             children: <Widget>[
+              
+              
               Padding(
                 padding: const EdgeInsets.fromLTRB(10, 0, 10, 10),
                 child: Text(
                   "Made with ❤ by Open Source",
                   style: TextStyle(
-                    color: Colors.blue[600],
+                    color: Colors.black,
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
                   ),
