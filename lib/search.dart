996c5e0d56291f84ac5d74a464326c3eb4a2f7db
Vrushti24 <vrushtishah24@gmail.com>
2021-01-20 22:56:25 +0530

    Search_feature

---
+++ lib/search.dart
@@ -0,0 +1,87 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_file_manager/flutter_file_manager.dart';
+import 'package:path_provider_ex/path_provider_ex.dart';
+
+
+
+
+class MyFileList extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _MyFileList();
+  }
+}
+
+class _MyFileList extends State<MyFileList>{
+  var files;
+   bool typing = false;
+
+ 
+  void getFiles() async { //asyn function to get list of files
+      List<StorageInfo> storageInfo = await PathProviderEx.getStorageInfo();
+      var root = storageInfo[0].rootDir; //storageInfo[1] for SD card, geting the root directory
+      var fm = FileManager(root: Directory(root)); //
+      files = await fm.filesTree( 
+      //set fm.dirsTree() for directory/folder tree list
+        excludedPaths: ["/storage/emulated/0/Android"],
+        extensions: ["png", "pdf"] //optional, to filter files, remove to list all,
+        //remove this if your are grabbing folder list
+      );
+      setState(() {}); //update the UI
+  }
+
+  @override
+  void initState() {
+    getFiles(); //call getFiles() function on initial state. 
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: typing ? TextBox() : Text("Search files"),
+        leading: IconButton(
+          icon: Icon(typing ? Icons.done : Icons.search),
+          onPressed: () {
+            setState(() {
+              typing = !typing;
+            });
+          },
+        ),
+
+        backgroundColor: Colors.black
+      ),
+      body:files == null? Text("                           Searching Files"):
+           ListView.builder(  //if file/folder list is grabbed, then show here
+              itemCount: files?.length ?? 0,
+              itemBuilder: (context, index) {
+                    return Card(
+                      child:ListTile(
+                         title: Text(files[index].path.split('/').last),
+                         leading: Icon(Icons.image),
+                         trailing: Icon(Icons.delete, color: Colors.redAccent,),
+                      )
+                    );
+              },
+          )
+    );
+  }
+}
+
+class TextBox extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      color: Colors.white,
+      child: TextField(
+        decoration:
+            InputDecoration(border: InputBorder.none, hintText: 'Search'),
+      ),
+    );
+  }
+}
+
