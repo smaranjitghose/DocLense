bf8120a49432cce0e8732181f2f86c1b08eace31
Truptimayee Sahu <truptimayee768200@gmail.com>
2021-01-31 20:28:19 +0530

    drawer_enhancement

--- lib/MainDrawer.dart
+++ lib/MainDrawer.dart
@@ -1,17 +1,29 @@
 import 'package:flutter/material.dart';
+
 import 'Home.dart';
 import 'About.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 
-class MainDrawer extends StatelessWidget {
+class MainDrawer extends StatefulWidget {
+  @override
+  _MainDrawerState createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {
   @override
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
         children: <Widget>[
           Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(18),
+                  bottomRight: Radius.circular(18)),
+              color: Colors.blue[600],
+            ),
             width: double.infinity,
             padding: EdgeInsets.all(20),
-            color: Colors.blue[600],
             child: Center(
                 child: Column(
               children: <Widget>[
@@ -20,7 +32,7 @@ class MainDrawer extends StatelessWidget {
                   height: 180,
                   child: CircleAvatar(
                     backgroundColor: Colors.white,
-                    radius: 60,
+                    radius: 20,
                     child: Image.asset(
                       'assets/images/scanlogo.png',
                     ),
@@ -28,52 +40,92 @@ class MainDrawer extends StatelessWidget {
                 ),
                 Text(
                   "One Place For All \n Your Documents!",
-                  style: TextStyle(fontSize: 20, color: Colors.white),
-                )
+                  style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Open Sans'),
+                ),
               ],
             )),
           ),
           SizedBox(
-            height: 10,
+            height: 20,
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => Home()));
-            },
-            leading: Icon(Icons.home),
-            title: Text(
-              "Home",
-              style: TextStyle(fontSize: 18),
+          Card(
+            child: ListTile(
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => Home()));
+              },
+              leading: Icon(
+                EvaIcons.home,
+                color: Colors.black,
+              ),
+              title: Text(
+                "Home",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 15,
+              ),
             ),
           ),
-          ListTile(
-            onTap: () {},
-            leading: Icon(Icons.star),
-            title: Text(
-              "Starred Documents",
-              style: TextStyle(fontSize: 18),
+          Card(
+            child: ListTile(
+              onTap: () {},
+              leading: Icon(
+                EvaIcons.star,
+                color: Colors.green,
+              ),
+              title: Text(
+                "Starred Documents",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 15,
+              ),
             ),
           ),
-          ListTile(
-            onTap: () {
-              Navigator.of(context).pop();
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => About()));
-            },
-            leading: Icon(Icons.info),
-            title: Text(
-              "About App",
-              style: TextStyle(fontSize: 18),
+          Card(
+            child: ListTile(
+              onTap: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (context) => About()));
+              },
+              leading: Icon(
+                EvaIcons.info,
+                color: Colors.lightBlue,
+              ),
+              title: Text(
+                "About App",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 15,
+              ),
             ),
           ),
-          ListTile(
-            onTap: () {},
-            leading: Icon(Icons.settings),
-            title: Text(
-              "Settings",
-              style: TextStyle(fontSize: 18),
+          Card(
+            child: ListTile(
+              onTap: () {},
+              leading: Icon(
+                EvaIcons.settings,
+                color: Colors.grey,
+              ),
+              title: Text(
+                "Settings",
+                style: TextStyle(fontSize: 18),
+              ),
+              trailing: Icon(
+                Icons.arrow_forward_ios,
+                size: 15,
+              ),
             ),
           ),
         ],
