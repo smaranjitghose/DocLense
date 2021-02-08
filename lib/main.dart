657d1701966bce56a416e55946e280263d9e230f
Nitesh Kumar <niteshkrraj2@gmail.com>
2021-02-08 11:31:52 +0530

    installationTimeReduced

--- lib/main.dart
+++ lib/main.dart
@@ -4,25 +4,21 @@ import 'package:flutter/material.dart';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'dart:ui';
 
-
 void main() {
   runApp(new MaterialApp(
+    debugShowCheckedModeBanner: false,
     home: MyApp(),
   ));
 }
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: AnimatedSplashScreen(
-        splash: Image.asset('assets/images/logos.png'),
-        nextScreen: Home(),
-        splashTransition: SplashTransition.rotationTransition,
-        duration: 4000,
-      ),
+    return AnimatedSplashScreen(
+      splash: Image.asset('assets/images/logos.png'),
+      nextScreen: Home(),
+      splashTransition: SplashTransition.rotationTransition,
+      duration: 4000,
     );
   }
 }
