ead69884005f3defe6867ed4ddb2f803fd1671a6
Saransh <saransh0701@gmail.com>
2021-02-08 22:11:47 +0530

    Updated Imageview.dart with both the themes

--- lib/Imageview.dart
+++ lib/Imageview.dart
@@ -2,9 +2,11 @@ import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:image_cropper/image_cropper.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
 import 'Filterview.dart';
 import 'Home.dart';
 import 'Providers/ImageList.dart';
+import 'Providers/ThemeProvider.dart';
 
 class Imageview extends StatefulWidget {
   File file;
@@ -73,130 +75,135 @@ class _ImageviewState extends State<Imageview> {
 
   @override
   Widget build(BuildContext context) {
+    final themeChange = Provider.of<DarkThemeProvider>(context);
+
     // TODO: implement build
     return Scaffold(
       body: SafeArea(
         child: Container(
-          color: Colors.blueGrey[100],
+          // color: Colors.blueGrey[100],
           child: Column(
             children: <Widget>[
               Expanded(
-                flex: 4,
+                flex: 9,
                 child: Padding(
                   padding: const EdgeInsets.fromLTRB(20, 10, 20, 20),
                   child: Image.file(files[index]),
                 ),
               ),
-              SafeArea(
-                child: Expanded(
-                  flex: 1,
-                  child: Container(
-                    height: 65,
-                    color: Colors.blue[600],
-                    child: Padding(
-                      padding: const EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: <Widget>[
-                          FlatButton(
-                            onPressed: () {
-                              Navigator.of(context).pop();
+              // SafeArea(
+              //   child:
+              Expanded(
+                flex: 1,
+                child: Container(
+                  height: 65,
+                  color: themeChange.darkTheme ? Colors.black87 : Colors
+                      .blue[600],
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        FlatButton(
+                          onPressed: () {
+                            Navigator.of(context).pop();
 //                                  Navigator.of(context).pushReplacement(MaterialPageRoute(
 //                                      builder: (context) => Home()));
-                            },
-                            color: Colors.blue[600],
-                            child: Column(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.arrow_back,
-                                  color: Colors.white,
-                                ),
-                                Text(
-                                  "Back",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
+                          },
+                          // color: Colors.blue[600],
+                          child: Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.arrow_back,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                "Back",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                          FlatButton(
-                            onPressed: () {
+                        ),
+                        FlatButton(
+                          onPressed: () {
 //                                  Navigator.of(context).pop();
-                              if (index == 0) {
-                              } else {
-                                setState(() {
-                                  index--;
-                                  files.removeLast();
-                                });
-                              }
-                            },
-                            color: Colors.blue[600],
-                            child: Column(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.undo,
-                                  color: Colors.white,
-                                ),
-                                Text(
-                                  "Undo",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
+                            if (index == 0) {} else {
+                              setState(() {
+                                index--;
+                                files.removeLast();
+                              });
+                            }
+                          },
+                          // color: Colors.blue[600],
+                          child: Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.undo,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                "Undo",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                          FlatButton(
-                            onPressed: () {
-                              cropimage(widget.file);
-                            },
-                            color: Colors.blue[600],
-                            child: Column(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.crop_rotate,
-                                  color: Colors.white,
-                                ),
-                                Text(
-                                  "Crop",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            cropimage(widget.file);
+                          },
+                          // color: Colors.blue[600],
+                          child: Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.crop_rotate,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                "Crop",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                          FlatButton(
-                            onPressed: () {
-                              if (cropped != null) {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => filter_image(
-                                            cropped, widget.list)));
-                              } else {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => filter_image(
-                                            widget.file, widget.list)));
-                              }
-                            },
-                            color: Colors.blue[600],
-                            child: Column(
-                              children: <Widget>[
-                                Icon(
-                                  Icons.arrow_forward,
-                                  color: Colors.white,
-                                ),
-                                Text(
-                                  "Next",
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ],
-                            ),
+                        ),
+                        FlatButton(
+                          onPressed: () {
+                            if (cropped != null) {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          filter_image(
+                                              cropped, widget.list)));
+                            } else {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          filter_image(
+                                              widget.file, widget.list)));
+                            }
+                          },
+                          // color: Colors.blue[600],
+                          child: Column(
+                            children: <Widget>[
+                              Icon(
+                                Icons.arrow_forward,
+                                color: Colors.white,
+                              ),
+                              Text(
+                                "Next",
+                                style: TextStyle(color: Colors.white),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
+              // ),
             ],
           ),
         ),
