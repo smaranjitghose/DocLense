b23f1e10efcd36d4f4e450b43d1b6df688d4c6cc
Sapinder Singh <sapindersingh1231@gmail.com>
2021-03-13 22:17:50 +0530

    fix(Whole Project): fixed imports
    fixed typos in imports.
    
    fixes issue 160

--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -1,15 +1,14 @@
-import 'package:doclense/starred_documents.dart';
 import 'package:doclense/settings.dart';
+import 'package:doclense/starred_documents.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+import 'package:rating_dialog/rating_dialog.dart';
 import 'package:share/share.dart';
 import 'package:url_launcher/url_launcher.dart';
-import 'package:rating_dialog/rating_dialog.dart';
-import 'package:provider/provider.dart';
 
 import 'about.dart';
-
-import 'Providers/theme_provider.dart';
+import 'providers/theme_provider.dart';
 
 class MainDrawer extends StatelessWidget {
   @override
