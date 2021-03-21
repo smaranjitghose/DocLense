7a3996a8b5d8c22b1bfcafa74efc4d0e8b03c693
Nikki Goel <gupta.nikkigupta@gmail.com>
2021-03-21 14:02:38 +0530

    UI contact team screen
    added box shadows around icons and images, changed the shape to circle
    
    fixes #190

--- lib/settings/contact_developers.dart
+++ lib/settings/contact_developers.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:url_launcher/url_launcher.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ContactDeveloperScreen extends StatelessWidget {
   final double spacing = 40;
@@ -10,47 +9,123 @@ class ContactDeveloperScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(height: spacing),
-                const Text(
-                  'CONTACT US',
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                SizedBox(height: spacing),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    _buildProfile(
-                      imagePath: 'assets/images/team/smaranjit_ghose.png',
-                      name: 'Smaranjit Ghose',
-                      githubUrl: 'smaranjitghose',
-                      linkedInUrl: 'smaranjitghose',
+          child: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.only(top: 30),
+          child: Column(
+            children: <Widget>[
+              Container(
+                alignment: Alignment.topCenter,
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const Text(
+                      "TEAM",
+                      style:
+                          TextStyle(fontSize: 25, fontWeight: FontWeight.w700),
                     ),
-                    _buildProfile(
-                      imagePath: 'assets/images/team/anush_bhatia.png',
-                      name: 'Anush Bhatia',
-                      githubUrl: 'anushbhatia',
-                      linkedInUrl: 'anushbhatia',
-                    ),
+                    Container(
+                      margin: const EdgeInsets.only(left: 8),
+                      child: const Text(
+                        "2021",
+                        style: TextStyle(
+                          fontSize: 25,
+                          fontWeight: FontWeight.w700,
+                          // color: Colors.orange[900],
+                        ),
+                      ),
+                    )
                   ],
                 ),
-                SizedBox(height: spacing),
-                const Text(
-                  'CONTRIBUTORS',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.w600,
-                  ),
+              ),
+              Container(
+                margin: const EdgeInsets.only(top: 15),
+                alignment: Alignment.topCenter,
+                child: const Text(
+                  "Presenting The Team",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
                 ),
-              ],
+              ),
+              _buildTeamTitle('DEVELOPERS'),
+              Container(
+                height: 170,
+                margin: const EdgeInsets.only(top: 20),
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemCount: devTeam.length,
+                    itemBuilder: (BuildContext context, int index) => Container(
+                          margin: const EdgeInsets.only(left: 15, right: 15),
+                          child: Column(
+                            children: <Widget>[
+                              _buildprofileImage(devTeam[index]["imgPath"]),
+                              Container(
+                                margin: const EdgeInsets.only(top: 10),
+                                child: Column(
+                                  children: <Widget>[
+                                    Text(
+                                      devTeam[index]["name"],
+                                    ),
+                                    const Padding(
+                                        padding: EdgeInsets.only(top: 5)),
+                                    Row(
+                                      children: <Widget>[
+                                        _buildProfileIcon(
+                                            devTeam[index]["linkedin"],
+                                            'https://img.icons8.com/fluent/48/000000/linkedin-circled.png'),
+                                        const Padding(
+                                            padding: EdgeInsets.only(left: 10)),
+                                        _buildProfileIcon(
+                                            devTeam[index]["github"],
+                                            'https://img.icons8.com/fluent/50/000000/github.png'),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        )),
+              ),
+              _buildTeamTitle('CONTIBUTORS'),
+            ],
+          ),
+        ),
+      )),
+    );
+  }
+
+  Widget _buildprofileImage(String imagePath) {
+    return Container(
+      width: 100.0,
+      height: 100.0,
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            offset: const Offset(0.00, 3.00),
+            color: const Color(0xff000000).withOpacity(0.16),
+            blurRadius: 6,
+          ),
+        ],
+        shape: BoxShape.circle,
+      ),
+      child: SizedBox(
+        width: 100,
+        height: 100,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            boxShadow: <BoxShadow>[
+              BoxShadow(
+                offset: const Offset(0.00, 3.00),
+                color: const Color(0xff000000).withOpacity(0.16),
+                blurRadius: 6,
+              ),
+            ],
+            color: Colors.black,
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: AssetImage(imagePath),
+              fit: BoxFit.cover,
             ),
           ),
         ),
@@ -58,62 +133,56 @@ class ContactDeveloperScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildProfile({
-    @required String imagePath,
-    @required String name,
-    @required String githubUrl,
-    @required String linkedInUrl,
-  }) {
-    const double height = 12;
-    const double textSize = 18;
-
-    return Column(
-      children: [
-        SizedBox(
-          height: 150,
-          width: 150,
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.asset(
-                imagePath,
-                fit: BoxFit.contain,
-              )),
+  Widget _buildTeamTitle(String title) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      alignment: Alignment.center,
+      child: Text(
+        title,
+        style: const TextStyle(
+          fontSize: 18,
+          fontWeight: FontWeight.w700,
         ),
-        const SizedBox(height: height),
-        Text(
-          name,
-          style: const TextStyle(
-            fontSize: textSize,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        const SizedBox(height: height),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            _profileLinks(
-              'Github',
-              'https://github.com/$githubUrl',
-              FontAwesomeIcons.githubSquare,
-            ),
-            const SizedBox(width: 40),
-            _profileLinks(
-              'LinkedIn',
-              'https://www.linkedin.com/in/$linkedInUrl',
-              FontAwesomeIcons.linkedin,
-            ),
-          ],
-        ),
-      ],
+      ),
     );
   }
 
-  Widget _profileLinks(String text, String link, IconData icon) => InkWell(
-      onTap: () => _launchURL(link),
-      child: FaIcon(
-        icon,
-        size: 40,
-      ));
+  Widget _buildProfileIcon(String link, String iconUrl) {
+    return Container(
+      width: 30.0,
+      height: 30.0,
+      decoration: BoxDecoration(
+        boxShadow: <BoxShadow>[
+          BoxShadow(
+            offset: const Offset(0.00, 3.00),
+            color: const Color(0xff000000).withOpacity(0.16),
+            blurRadius: 6,
+          ),
+        ],
+        shape: BoxShape.circle,
+      ),
+      child: RawMaterialButton(
+        shape: const CircleBorder(),
+        elevation: 10,
+        onPressed: () async {
+          await _launchURL(link);
+        },
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                image: NetworkImage(iconUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
 
   Future<void> _launchURL(String url) async {
     if (await canLaunch(url)) {
@@ -124,3 +193,18 @@ class ContactDeveloperScreen extends StatelessWidget {
     }
   }
 }
+
+List<Map<String, String>> devTeam = [
+  {
+    "imgPath": 'assets/images/team/smaranjit_ghose.png',
+    "name": "Smaranjit Ghose",
+    "linkedin": "https://www.linkedin.com/in/smaranjitghose/",
+    "github": "https://github.com/smaranjitghose",
+  },
+  {
+    "imgPath": "assets/images/team/anush_bhatia.png",
+    "name": "Anush Bhatia",
+    "linkedin": "https://www.linkedin.com/in/anush-bhatia-aa500a158/",
+    "github": "https://github.com/anushbhatia",
+  },
+];
