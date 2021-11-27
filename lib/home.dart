1d1667986999ab52a63cf8ffd59156f0d8f2983b
Ayush Yadav <ayushiit2003@gmail.com>
2021-11-28 04:49:30 +0530

    removed linting error

--- lib/home.dart
+++ lib/home.dart
@@ -638,77 +638,69 @@ class _HomeState extends State<Home> {
                                                   newPath =
                                                       '${folderDir.path}/${(pdfsBox.getAt(0)[index][0] as String).split('/').last}';
                                                   print(newPath);
-                                                  if (newPath != null) {
-                                                    final List<dynamic>
-                                                        starred =
-                                                        Hive.box('starred')
-                                                                .getAt(0)
-                                                            as List<dynamic>;
-                                                    final List<dynamic>
-                                                        finalStarred = [];
+                                                  final List<dynamic> starred =
+                                                      Hive.box('starred')
+                                                              .getAt(0)
+                                                          as List<dynamic>;
+                                                  final List<dynamic>
+                                                      finalStarred = [];
+                                                  for (int i = 0;
+                                                      i < starred.length;
+                                                      i++) {
+                                                    finalStarred
+                                                        .add(starred[i][0]);
+                                                  }
+                                                  final File sourceFile =
+                                                      File(oldPath);
+                                                  if (finalStarred.contains(
+                                                      pdfsBox.getAt(0)[index]
+                                                          [0])) {
+                                                    print('yes');
                                                     for (int i = 0;
-                                                        i < starred.length;
+                                                        i < finalStarred.length;
                                                         i++) {
-                                                      finalStarred
-                                                          .add(starred[i][0]);
-                                                    }
-                                                    final File sourceFile =
-                                                        File(oldPath);
-                                                    if (finalStarred.contains(
-                                                        pdfsBox.getAt(0)[index]
-                                                            [0])) {
-                                                      print('yes');
-                                                      for (int i = 0;
-                                                          i <
-                                                              finalStarred
-                                                                  .length;
-                                                          i++) {
-                                                        if (Hive.box('starred')
-                                                                    .getAt(0)[i]
-                                                                [0] ==
-                                                            sourceFile.path) {
-                                                          print('yes');
-                                                          await sourceFile
-                                                              .copy(newPath);
-                                                          await sourceFile
-                                                              .delete();
+                                                      if (Hive.box('starred')
+                                                              .getAt(0)[i][0] ==
+                                                          sourceFile.path) {
+                                                        print('yes');
+                                                        await sourceFile
+                                                            .copy(newPath);
+                                                        await sourceFile
+                                                            .delete();
 
-                                                          Hive.box('starred')
-                                                                  .getAt(0)[i]
-                                                              [0] = newPath;
-                                                          pdfsBox.getAt(
-                                                                  0)[index][0] =
-                                                              newPath;
-                                                          final List<dynamic>
-                                                              editedList =
-                                                              Hive.box('starred')
-                                                                      .getAt(0)
-                                                                  as List<
-                                                                      dynamic>;
-                                                          Hive.box('starred')
-                                                              .putAt(0,
-                                                                  editedList);
-                                                          final List<dynamic>
-                                                              pdfEditedList =
-                                                              pdfsBox.getAt(0)
-                                                                  as List<
-                                                                      dynamic>;
-                                                          pdfsBox.putAt(
-                                                              0, pdfEditedList);
-                                                          break;
-                                                        }
-                                                      }
-                                                    } else {
-                                                      print(
-                                                          "Newpath: $newPath");
-                                                      await sourceFile
-                                                          .copy(newPath);
-                                                      await sourceFile.delete();
-                                                      setState(() {
+                                                        Hive.box('starred')
+                                                                .getAt(0)[i]
+                                                            [0] = newPath;
                                                         pdfsBox.getAt(0)[index]
                                                             [0] = newPath;
-                                                      });
+                                                        final List<dynamic>
+                                                            editedList =
+                                                            Hive.box('starred')
+                                                                    .getAt(0)
+                                                                as List<
+                                                                    dynamic>;
+                                                        Hive.box('starred')
+                                                            .putAt(
+                                                                0, editedList);
+                                                        final List<dynamic>
+                                                            pdfEditedList =
+                                                            pdfsBox.getAt(0)
+                                                                as List<
+                                                                    dynamic>;
+                                                        pdfsBox.putAt(
+                                                            0, pdfEditedList);
+                                                        break;
+                                                      }
                                                     }
+                                                  } else {
+                                                    print("Newpath: $newPath");
+                                                    await sourceFile
+                                                        .copy(newPath);
+                                                    await sourceFile.delete();
+                                                    setState(() {
+                                                      pdfsBox.getAt(0)[index]
+                                                          [0] = newPath;
+                                                    });
                                                   }
                                                 }
                                               },
