b4a8d9435650c550fcc2764787bfff1f06755a75
kururu-abdo <wdalhajana50@gmail.com>
2023-07-23 00:57:35 +0300

    git rid of erros in test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:doclense/main.dart';
 import 'package:flutter/material.dart';
+// import 'package:test/test.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
