2653523911c4d6ecd9ba4829809d70b092016c0f
Jinosh05 <jinoshprabhuraj5@gmail.com>
2023-09-12 22:38:30 +0530

    GridItem rules applied

--- lib/ui_components/grid_item.dart
+++ lib/ui_components/grid_item.dart
@@ -1,8 +1,7 @@
 import "package:doclense/configs/app_dimensions.dart";
 import "package:doclense/configs/space.dart";
-import "package:flutter/material.dart";
-
 import "package:doclense/ui_components/multi_select_delete.dart";
+import "package:flutter/material.dart";
 
 class GridItem extends StatefulWidget {
   const GridItem({required this.item, required this.isSelected, super.key});
@@ -11,45 +10,45 @@ class GridItem extends StatefulWidget {
   final Item item;
 
   @override
-  _GridItemState createState() => _GridItemState();
+  GridItemState createState() => GridItemState();
 }
 
-class _GridItemState extends State<GridItem> {
+class GridItemState extends State<GridItem> {
   bool isSelected = false;
 
   @override
   Widget build(BuildContext context) => InkWell(
-      onTap: () {
-        setState(() {
-          isSelected = !isSelected;
-          widget.isSelected(isSelected);
-        });
-      },
-      child: Stack(
-        children: <Widget>[
-          Image.file(
-            widget.item.imageUrl,
-            color: Colors.black.withOpacity(isSelected ? 0.9 : 0),
-            colorBlendMode: BlendMode.color,
-            fit: BoxFit.fill, //Determines the size ratio of the gridimage
-            width: AppDimensions.width(50),
-            height: AppDimensions.width(50),
-          ),
-          if (isSelected)
-            Container(
-              alignment: Alignment.bottomRight,
-              padding: Space.all(),
-              child: Icon(
-                Icons.check_circle,
-                color: Colors.black,
-                size: AppDimensions.font(
-                  11,
+        onTap: () {
+          setState(() {
+            isSelected = !isSelected;
+            widget.isSelected(isSelected);
+          });
+        },
+        child: Stack(
+          children: <Widget>[
+            Image.file(
+              widget.item.imageUrl,
+              color: Colors.black.withOpacity(isSelected ? 0.9 : 0),
+              colorBlendMode: BlendMode.color,
+              fit: BoxFit.fill, //Determines the size ratio of the gridimage
+              width: AppDimensions.width(50),
+              height: AppDimensions.width(50),
+            ),
+            if (isSelected)
+              Container(
+                alignment: Alignment.bottomRight,
+                padding: Space.all(),
+                child: Icon(
+                  Icons.check_circle,
+                  color: Colors.black,
+                  size: AppDimensions.font(
+                    11,
+                  ),
                 ),
-              ),
-            )
-          else
-            Container(),
-        ],
-      ),
-    );
+              )
+            else
+              Container(),
+          ],
+        ),
+      );
 }
